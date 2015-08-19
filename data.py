85e792e7268a4a1b03a2f573d8c0b6ab94658484
Robert Rees <robert.rees@guardian.co.uk>
2015-08-19 17:52:55 +0100

    All the shortcodes!!

--- data.py
+++ data.py
@@ -3,4 +3,158 @@ codes_csv = """
 twt_gu,global twitter,1,Twitter,,,UK,,,,,UNIQUE
 fb_gu,global facebook,1,Facebook,,,UK,,,,Used for the main brand Facebook page,UNIQUE
 fb_us,us facebook,1,Facebook,,,US,US,,,,UNIQUE
+soc_567,australia facebook,1,Facebook,,,AU,AU,,,,UNIQUE
+fb_cif,facebook cif,1,Facebook,,commentisfree,,,,,,UNIQUE
+EMCMONTXT5510I2,Money Talks,1,Email,,money,,,,,,UNIQUE
+ema_1046,Close up,1,Email ,,film,,,,,,UNIQUE
+EMCEDUEML1658,Crib sheet,1,Email,,education,,,,,,UNIQUE
+EMCCRWEML1626,Crosswords,1,Email ,,crosswords,,,,,,UNIQUE
+ema_789,Fashion Statement,1,Email ,,fashion,,,,,,UNIQUE
+EMCENVEML1631,Green Light,1,Email ,,environment,,,,,,UNIQUE
+EMCNEWEML6619I2,Guardian Today - UK,1,Email ,,,UK,UK,,,,UNIQUE
+ema_632,Guardian Today - AUS,1,Email ,,,US,US,,,,UNIQUE
+ema_565,Guardian Today - USA,1,Email ,,,AU,AU,,,,UNIQUE
+EMCNEWEML1644,Guardian Weekly,1,Email ,,media,,,,,,UNIQUE
+ema_546,Media Briefing,1,Email ,,media,,,,,,UNIQUE
+EMCNEWEML1643,Metropolitan Lines,1,Email ,,politics,,,,,,UNIQUE
+EMCGBLEML1625,Poverty Matters,1,Email ,,global-development,,,,,,UNIQUE
+ema_630,SleeveNotes,1,Email ,,music,,,,,,UNIQUE
+EMCSOCEML657,Society Briefing,1,Email ,,society,,,,,,UNIQUE
+EMCSPTEML866,The Breakdown,1,Email ,,sport,,,,,,UNIQUE
+EMCFTBEML853,The Fiver,1,Email ,,football,,,,,,UNIQUE
+EMCTRVEML1657,The Flyer,1,Email ,,travel,,,,,,UNIQUE
+EMCARTEML6852,Art Weekly,1,Email ,,artanddesign,,,,,,UNIQUE
+EMCBKSEML3964,Book Club,1,Email ,,books,,,,,,UNIQUE
+EMCSPTEML942,The Spin,1,Email ,,sport,,,,,,UNIQUE
+ema_792,AUS Politics,1,Email ,,politics,AU,AU,,,,UNIQUE
+ema_827,Zip file,1,Email ,,technology,,,,,,UNIQUE
+ema_1364,Best of Comment is Free,1,Email ,,commentisfree,,,,,,UNIQUE
+ema_861,Film Today,1,Email ,,film,,,,,,UNIQUE
+ema_1731,Australia Morning Mail,1,Email ,,,AU,AU,,,,UNIQUE
+ema_1732,First Dog on the Moon,1,Email ,,commentisfree,AU,AU,,,,UNIQUE
+ema_2313,Best of Comment is Free - Australia,1,Email ,,commentisfree,AU,AU,,,,UNIQUE
+tmb_gu,Guardian Tumblr,1,Tumblr,,,,,,,,UNIQUE
+twt_tc,The Counted,1,Twitter,,,US,,,,,UNIQUE
+fb_tc,The Counted,,Facebook,,,US,,,,,UNIQUE
+soc_3157,gdncif,,Tumblr,,commentisfree,,,,,,UNIQUE
+soc_3156,gdnopinion,,Twitter,,commentisfree,,,,,,UNIQUE
+soc_568,GuardianAus,,Twitter,,,AU,AU,,,,UNIQUE
+edit_2221,GuardianUs,,Twitter,,,US,US,,,,UNIQUE
+twt_a-sport_b-ussports_c-us_g-1,GdnUSsports,,Twitter,,sport,US,US,,,,UNIQUE
+twt_a-cities_b-gdncities,gdncities,,Twitter,,cities,,,,,,UNIQUE
+twt_books_b-gdnbooks,gdnbooks,,Twitter,,books,,,,,,UNIQUE
+twt_a-travel_b-gdntravel,gdntravel,,Twitter,,travel,,,,,,UNIQUE
+twt_business_b-gdnbiz,gdnbiz,,Twitter,,business,,,,,,UNIQUE
+twt_a-film_b-gdnfilm,gdnfilm,,Twitter,,film,,,,,,UNIQUE
+twt_a-environment_b-gdneco,gdneco,,Twitter,,environment,,,,,,UNIQUE
+twt_a-media_b-gdnmedia,gdnmedia,,Twitter,,media,,,,,,UNIQUE
+twt_a-science_b-gdnscience,gdnscience,,Twitter,,science,,,,,,UNIQUE
+twt_b-gdnnews,gdnnews,,Twitter,,,,,,,,UNIQUE
+twt_a-music_b-gdnmusic,gdnmusic,,Twitter,,music,,,,,,UNIQUE
+twt_a-education_b-gdnedu,gdnedu,,Twitter,,education,,,,,,UNIQUE
+twt_a-world_b-gdnworld,gdnworld,,Twitter,,world,,,,,,UNIQUE
+twt_a-education_b-gdnstudents,gdnstudents,,Twitter,,education,,,,,,UNIQUE
+twt_a-technology_b-gdntech,gdntech,,Twitter,,technology,,,,,,UNIQUE
+twt_a-sport_b-gdnsport,gdnsport,,Twitter,,sport,,,,,,UNIQUE
+twt_a-lifeandstyle_b-gdnlifeandstyle,gdnlifeandstyle,,Twitter,,lifeandstyle,,,,,,UNIQUE
+twt_b-gdnphotos,gdnphotos,,Twitter,,,,,,,,UNIQUE
+twt_a-global-development_b-gdndevelopment,gdndevelopment,,Twitter,,global-development,,,,,,UNIQUE
+twt_b-gdng2,gdng2,,Twitter,,,,,,,,UNIQUE
+twt_b-gdnguide,gdnguide,,Twitter,,,,,,,,UNIQUE
+twt_b-gdnweekly,gdnweekly,,Twitter,,,,,,,,UNIQUE
+twt_a-money_b-gdnmoney,gdnmoney,,Twitter,,money,,,,,,UNIQUE
+twt_a-society_b-gdnsociety,gdnsociety,,Twitter,,society,,,,,,UNIQUE
+twt_b-obsnewreview,ObsNewReview,,Twitter,,,,,,,,UNIQUE
+twt_a-lifeandstyle_b-gdnfamily,gdnfamily,,Twitter,,lifeandstyle,,,,,,UNIQUE
+twt_b-gdnweekend,gdnweekend,,Twitter,,,,,,,,UNIQUE
+twt_a-culture_b-gdnculture,gdnculture,,Twitter,,culture,,,,,,UNIQUE
+twt_a-stage_b-gdnstage,gdnstage,,Twitter,,stage,,,,,,UNIQUE
+twt_b-gdnscotland,GdnScotland,,Twitter,,uk-news,,,,,,UNIQUE
+twt_a-gnm-archive_b-gdnlibrary,gdnlibrary,,Twitter,,gnm-archive,,,,,,UNIQUE
+twt_a-artanddesign_b-gdnartanddesign,gdnartanddesign,,Twitter,,artanddesign,,,,,,UNIQUE
+twt_b-obsmagazine,obsmagazine,,Twitter,,,,,,,,UNIQUE
+twt_a-childrens-books-site_b-gdnchildrenbks,GdnChildrensBks,,Twitter,,childrens-books-site,,,,,,UNIQUE
+twt_a-world_b-gdnafrica,gdnafrica,,Twitter,,world,,,,,,UNIQUE
+twt_b-gdnwitness,gdnwitness,,Twitter,,,,,,,,UNIQUE
+twt_b-gdnreview,gdnreview,,Twitter,,,,,,,,UNIQUE
+twt_b-gdnaudio,gdnaudio,,Twitter,,,,,,,,UNIQUE
+twt_a-lifeandstyle_b-gdnfood,gdnfood,,Twitter,,lifeandstyle,,,,,,UNIQUE
+twt_a-science_b-gdnsciblog,gdnsciblog,,Twitter,,science,,,,,,UNIQUE
+twt_a-football_b-thefiver,thefiver,,Twitter,,football,,,,,,UNIQUE
+twt_b-gdnobits,gdnobits,,Twitter,,(defaults to null),,,,,,UNIQUE
+twt_a-tv-and-radio_b-gdntv,gdntv,,Twitter,,tv-and-radio,,,,,,UNIQUE
+twt_a-lifeandstyle_b-gdngardens,gdngardens,,Twitter,,lifeandstyle,,,,,,UNIQUE
+twt_a-media_b-mgmediamonkey,mgmediamonkey,,Twitter,,media,,,,,,UNIQUE
+twt_a-tv-and-radio_b-gdnradio,gdnradio,,Twitter,,tv-and-radio,,,,,,UNIQUE
+twt_a-culture_b-gdnausculture_c-au_g-3,GdnAusCulture,,Twitter,,culture,AU,AU,,,,UNIQUE
+twt_b-gdnvideo,gdnvideo,,Twitter,,,,,,,,UNIQUE
+twt_a-politics_b-gdnukpolitics,gdnukpolitics,,Twitter,,politics,,,,,,UNIQUE
+twt_b-g2filmandmusic,G2FilmandMusic,,Twitter,,,,,,,,UNIQUE
+twt_b-gdnnortherner,GdnNortherner,,Twitter,,,,,,,,UNIQUE
+twt_a-fashion_b-gdnfashion,gdnfashion,,Twitter,,fashion,,,,,,UNIQUE
+twt_b-gdndata,gdndata,,Twitter,,,,,,,,UNIQUE
+twt_b-obstechmonthly,ObsTechMonthly,,Twitter,,,,,,,,UNIQUE
+twt_a-higher-education-network_b-gdnfe,gdnFE,,Twitter,,higher-education-network,,,,,,UNIQUE
+twt_a-sustainable-business_b-gdnsocent,gdnsocent,,Twitter,,sustainable-business,,,,,,UNIQUE
+twt_a-public-leaders-network_b-gdnpublic,gdnpublic,,Twitter,,public-leaders-network,,,,,,UNIQUE
+twt_a-higher-education_b-gdnhighered,GdnHigherEd,,Twitter,,higher-education,,,,,,UNIQUE
+twt_a-sport_b-gdnausport_c-au_g-3,GdnAusSport,,Twitter,,sport,AU,AU,,,,UNIQUE
+twt_a-lifeandstyle_b-gdnstyle,gdnstyle,,Twitter,,lifeandstyle,,,,,,UNIQUE
+twt_b-gdnarchive,GdnArchive,,Twitter,,,,,,,,UNIQUE
+twt_a-media-network_b-gdnmtn,gdnmtn,,Twitter,,media-network,,,,,,UNIQUE
+twt_b-gdncountrydiary,gdncountrydiary,,Twitter,,,,,,,,UNIQUE
+twt_a-world_b-gdnnk,GdnNK,,Twitter,,world,,,,,,UNIQUE
+twt_a-world_b-gdnneweast,gdnNewEast,,Twitter,,world,,,,,,UNIQUE
+twt_a-music_b-gdnclassical,GdnClassical,,Twitter,,music,,,,,,UNIQUE
+inst_a-cities_b-gdncities,gdncities,,Instagram,,cities,,,,,,UNIQUE
+inst_b-gdninsta,gdninsta,,Instagram,,,,,,,,UNIQUE
+inst_a-travel_b-gdntravelsnaps,gdntravelsnaps,,Instagram,,travel,,,,,,UNIQUE
+inst_b-gdnus_c-us_g-1,gdnUS,,Instagram,,,US,US,,,,UNIQUE
+inst_b-gdnau_c-au_g-3,gdnAU,,Instagram,,,AU,AU,,,,UNIQUE
+inst_b-gdncam,gdncam,,Instagram,,,,,,,,UNIQUE
+fb_b-obsnewreview,obsnewreview,,Facebook,,,,,,,,UNIQUE
+fb_a-football_b-gdnfootball,gdnfootball,,Facebook,,football,,,,,,UNIQUE
+fb_a-culture_b-gdnculture,gdnculture,,Facebook,,culture,,,,,,UNIQUE
+fb_a-technology_b-gdntech,gdntechn,,Facebook,,technology,,,,,,UNIQUE
+fb_a-science_b-gdnscience,gdnscience,,Facebook,,science,,,,,,UNIQUE
+fb_a-media_b-gdnmedia,gdnmedia,,Facebook,,media,,,,,,UNIQUE
+fb_a-education_b-gdnedu,gdnedu,,Facebook,,education,,,,,,UNIQUE
+fb_a-music_b-gdnmusic,gdnmusic,,Facebook,,music,,,,,,UNIQUE
+fb_a-sport_b-gdnsport,gdnsport,,Facebook,,sport,,,,,,UNIQUE
+fb_a-film_b-gdnfilm,gdnfilm,,Facebook,,film,,,,,,UNIQUE
+fb_a-society_b-gdnsociety,gdnscociety,,Facebook,,society,,,,,,UNIQUE
+fb_a-environment_b-gdnenvironment,gdnenvironment,,Facebook,,environment,,,,,,UNIQUE
+fb_a-lifeandstyle_b-gdnlifeandstyle,gdnlifeandstyle,,Facebook,,lifeandstyle,,,,,,UNIQUE
+fb_a-travel_b-gdntravel,gdntravel,,Facebook,,travel,,,,,,UNIQUE
+fb_a-global-development_b-gdnglobaldev,gdnglobaldev,,Facebook,,global-development,,,,,,UNIQUE
+fb_b-gdndata,gdndata,,Facebook,,,,,,,,UNIQUE
+fb_a-fashion_b-gdnfashion,gdnfashion,,Facebook,,fashion,,,,,,UNIQUE
+fb_a-cities_b-gdncities,gdncities,,Facebook,,cities,,,,,,UNIQUE
+fb_a-higher-education-network_b-gdnstudents,gdnstudents,,Facebook,,higher-education-network,,,,,,UNIQUE
+fb_a-teacher-network_b-gdnteachernetwork,gdnteachernetwork,,Facebook,,teacher-network,,,,,,UNIQUE
+fb_a-tv-and-radio_b-gdntvandradio,gdntvandradio,,Facebook,,tv-and-radio,,,,,,UNIQUE
+fb_a-books_b-gdnteenbooks,gdnteenbooks,,Facebook,,books,,,,,,UNIQUE
+fb_a-sport_b-gdnussport,gdnussport,,Facebook,,sport,,,,,,UNIQUE
+fb_b-gdnweekly,gdnweekly,,Facebook,,,,,,,,UNIQUE
+fb_a-technology_b-gdngames,gdngames,,Facebook,,technology,,,,,,UNIQUE
+fb_a-global-development-network_b-gdnglobaldevprofnetwork,gdnglobaldevprofnetwork,,Facebook,,global-development-professionals-network,,,,,,UNIQUE
+fb_a-sustainable-business_b-gdnsustbiz,gdnsustbiz,,Facebook,,sustainable-business,,,,,,UNIQUE
+fb_a-social-enterprise-network_b-gdnsocialenterprisenetwork,gdnsocialenterprisenetwork,,Facebook,,social-enterprise-network,,,,,,UNIQUE
+fb_b-gdnwitness,gdnwitness,,Facebook,,,,,,,,UNIQUE
+fb_a-higher-education_b-gdnhighereducationnetwork,gdnhighereducationnetwork,,Facebook,,higher-education,,,,,,UNIQUE
+fb_a-culture-professionals-network_b-gdncultprofnetwork,gdncultprofnetwork,,Facebook,,culture-professionals-network,,,,,,UNIQUE
+fb_a-media_b-gdnstudentmediaawards,gdnstudentmediaawards,,Facebook,,media,,,,,,UNIQUE
+fb_a-voluntary-sector-network_b-gdnstudentmediaawards,gdnvoluntarysectornetwork,,Facebook,,voluntary-sector-network,,,,,,UNIQUE
+fb_a-healthcare-network_b-gdnhealthcarenetwork,gdnhealthcarenetwork,,Facebook,,healthcare-network,,,,,,UNIQUE
+fb_a-small-business-network_b-gdnsmallbiznetwork,gdnsmallbiznetwork,,Facebook,,small-business-network,,,,,,UNIQUE
+fb_a-media-network_b-gdnmedianetwork,gdnmedianetwork,,Facebook,,media-network,,,,,,UNIQUE
+fb_a-housing-network_b-gdnhousingnetwork,gdnhousingnetwork,,Facebook,,housing-network,,,,,,UNIQUE
+fb_a-culture_b-gdncultureau_c-au_g-3,gdncultureau,,Facebook,,culture,AU,AU,,,,UNIQUE
+tmb_a-music_b-gdnmusic,gdnmusic,,Tumblr,,music,,,,,,UNIQUE
+tmb_b-english2english_c-us,English2English,,Tumblr,,,US,,,,,UNIQUE
+tmb_a-football_b-sadfootballers,SadFootballers,,Tumblr,,football,,,,,,UNIQUE
+ons_b-richmapper,richmapper,,On-site,,,,,,,,UNIQUE
+fb_b-firstdog_c-au_g-3,firstdog,,Facebook,,,AU,,,,,UNIQUE
+twt_b-cifaus_c-au_g-3,cifaus,,Twitter,,commentisfree,AU,AU,,,,UNIQUE
+twt_b-realitycheck,realitycheck,,Twitter,,,,,,,,UNIQUE
 """
