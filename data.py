5a28cf501739cbb35de381fb494b91d8d3c52204
Robert Rees <robert.rees@guardian.co.uk>
2015-09-05 20:15:03 +0100

    Updates the short code table to the be the latest and after a lot of faffing around reads an optional query param that has a url in it

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-
+# campaign_code,name,variant,campaign_type,placement,campaign_section,office,territory_targeted,paid_organic,owner,long_description,is unique helper column 
 codes_csv = """
 twt_gu,global twitter,1,Twitter,,,UK,,,,,UNIQUE
 fb_gu,global facebook,1,Facebook,,,UK,,,,Used for the main brand Facebook page,UNIQUE
