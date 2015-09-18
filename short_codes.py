530419d5cebc6f8394412afdb517dd9a3fd20c8b
Robert Rees <robert.rees@guardian.co.uk>
2015-09-18 19:24:58 +0100

    Avoids confusing Angular by duplicating entries in the list

--- short_codes.py
+++ short_codes.py
@@ -4,6 +4,7 @@ import StringIO
 import data
 
 columns = {
+	'campaign': 3,
 	'section': 5,
 }
 
@@ -31,6 +32,18 @@ def valid_short_code(section, short_code_row):
 
 	return False
 
+def general_short_code(short_code_row):
+
+	campaign_section = short_code_row[columns['section']]
+	campaign_code = short_code_row[columns['campaign']]
+
+	valid_campaigns = {'Twitter', 'Facebook', 'Instagram', 'Tumblr'}
+
+	if campaign_code in valid_campaigns and not campaign_section:
+		return True
+
+	return False
+
 def section_specific_codes(section, short_code_data):
 	if not section:
 		return []
@@ -40,6 +53,6 @@ def codes(section=None):
 
 	all_codes = read_codes()
 
-	codes = [output_data(row) for row in all_codes if valid_short_code(section, row)]
+	general_codes = [output_data(row) for row in all_codes if general_short_code(row)]
 
-	return section_specific_codes(section, all_codes) + codes
+	return section_specific_codes(section, all_codes) + general_codes
