5e337bef89abc7f6aba58c062892014e1ad7be9e
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2016-09-06 18:17:26 +0100

    Pull in a csv related to the GA codes. Load codes if parameter view=ga

---
+++ data_ga.py
@@ -0,0 +1,7 @@
+# * mandatory
+# capaign_source*,campaign_medium*,campaign_name*,campaign_content,campaign_term,internal_campaign,section,comment
+codes_csv = """
+Facebook,PPC,Whatever - I hopeless at names,photo,top banner,internal-campaign,commentisfree,some comment
+Twitter,PPC,Whatever - I hopeless at names,photo,top banner,internal-campaign,commentisfree,some comment
+Facebook,PPC,Whatever - I hopeless at names,photo,top banner,internal-campaign,politics,some comment
+"""
